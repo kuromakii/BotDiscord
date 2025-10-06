d364c631b5b8174416f43778aeb7173f2db7ecfa
kuromakii <kuromaki29140@gmail.com>
2025-10-06 20:31:52 +0200

    Ajout de la réponse automatique au ping de Kuromakii

--- bot.js
+++ bot.js
@@ -25,11 +25,11 @@ client.on("messageCreate", (message) => {
     message.content.toLowerCase().includes("@kuromakii") // mention tapée manuellement
   ) {
     const réponses = [
-      "On ne ping pas Kuromakii. On dépose une offrande et on attend qu’il vienne.",
-      "Ping pas Kuromakii... il te regarde déjà.",
-      "Ping encore Kuromakii et c’est lui qui te pingera… depuis ta penderie.",
-      "Kuromakii ne répond pas aux pings. Il les hante.",
-      "Ping pas Kuromakii… le dernier qui l’a fait tape encore “désolé” en morse."
+      "On ne ping pas **James Liwis**. On dépose une offrande et on attend qu’il vienne.",
+      "Ping pas **James Liwis**... il te regarde déjà.",
+      "Ping encore **James Liwis** et c’est lui qui te pingera… depuis ta penderie.",
+      "**James Liwis** ne répond pas aux pings. Il les hante.",
+      "Ping pas **James Liwis**… le dernier qui l’a fait tape encore “désolé” en morse."
     ];
 
     const réponse = réponses[Math.floor(Math.random() * réponses.length)];
